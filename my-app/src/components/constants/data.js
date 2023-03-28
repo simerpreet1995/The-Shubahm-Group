a22fadf013de3edff4f389176a0ab1fa21a104f6
simer <simerpreet2844@gmail.com>
2023-03-28 08:17:31 -0400

    remove carousel/banner/slidder

--- my-app/src/components/constants/data.js
+++ my-app/src/components/constants/data.js
@@ -1,28 +1,28 @@
-import Banner1 from '../constants/omaxe_banner1.jpg'
-import Banner2 from '../constants/omaxe_banner2.jpg'
-import Banner3 from '../constants/omaxe_banner3.jpg'
-import Banner4 from '../constants/omaxe_banner4.jpg'
-import Banner5 from '../constants/omaxe_banner5.jpg'
-import Banner6 from '../constants/omaxe_banner6.jpg'
+// import Banner1 from '../constants/omaxe_banner1.jpg'
+// import Banner2 from '../constants/omaxe_banner2.jpg'
+// import Banner3 from '../constants/omaxe_banner3.jpg'
+// import Banner4 from '../constants/omaxe_banner4.jpg'
+// import Banner5 from '../constants/omaxe_banner5.jpg'
+// import Banner6 from '../constants/omaxe_banner6.jpg'
 
 
 
 
 
-export const bannerData = [
-    // { id: 1, url: 'https://rukminim1.flixcart.com/flap/3376/560/image/d117a62eb5fbb8e1.jpg?q=50' },
-    // { id: 2, url: 'https://rukminim1.flixcart.com/flap/3376/560/image/57267a180af306fe.jpg?q=50' },
-    // { id: 3, url: 'https://rukminim1.flixcart.com/flap/3376/560/image/ae9966569097a8b7.jpg?q=50' },
-    // { id: 4, url: 'https://rukminim1.flixcart.com/flap/3376/560/image/f6202f13b6f89b03.jpg?q=50' },
+// export const bannerData = [
+//     // { id: 1, url: 'https://rukminim1.flixcart.com/flap/3376/560/image/d117a62eb5fbb8e1.jpg?q=50' },
+//     // { id: 2, url: 'https://rukminim1.flixcart.com/flap/3376/560/image/57267a180af306fe.jpg?q=50' },
+//     // { id: 3, url: 'https://rukminim1.flixcart.com/flap/3376/560/image/ae9966569097a8b7.jpg?q=50' },
+//     // { id: 4, url: 'https://rukminim1.flixcart.com/flap/3376/560/image/f6202f13b6f89b03.jpg?q=50' },
 
-    {id: 1,  url: Banner1 },
-    {id: 2,  url: Banner2 },
-    {id: 3,  url: Banner3 },
-    {id: 4,  url: Banner4 },
-    {id: 5,  url: Banner5 },
-    {id: 6,  url: Banner6 },
+//     {id: 1,  url: Banner1 },
+//     {id: 2,  url: Banner2 },
+//     {id: 3,  url: Banner3 },
+//     {id: 4,  url: Banner4 },
+//     {id: 5,  url: Banner5 },
+//     {id: 6,  url: Banner6 },
 
-]
+// ]
 
 
 
--- my-app/src/components/landingpage/header/Home.jsx
+++ my-app/src/components/landingpage/header/Home.jsx
@@ -1,14 +1,9 @@
-import Carousel from 'react-multi-carousel'
-import { bannerData } from '../../constants/data';
-import styled from '@emotion/styled';
-import 'react-multi-carousel/lib/styles.css';
-import { Box, Grid, Typography} from '@mui/material';
+import { Box, Grid, Typography, styled} from '@mui/material';
 
 import BackgroundImg from "../../../images/background-video.mp4";
 import "./Home.css";
 import Logo from '../../../images/logo-the-shubham.png'
 import {cardData} from '../../constants/card'
-import { useState } from 'react';
 import CardContainer from './CardContainer';
 
 
@@ -31,56 +26,12 @@ const CardImage = styled('img')({
 
 
 
-const responsive = {
-    
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 1
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 1
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1
-  }
-};
 
 
 function Home() {
 
   return (
     <Box className="main-container">
-
-<Box className="banner">
-      <Carousel 
-        responsive={responsive}
-        swipeable={false}
-        draggable={false}
-        infinite={true}
-        autoPlay={true}
-        keyBoardControl={true}
-        autoPlaySpeed={3000}
-        customTransition="all .1"
-        transitionDuration={500}
-        slidesToSlide={1}
-        dotListClass="custom-dot-list-style"
-        itemClass="carousel-item-padding-40-px"
-        containerClass="carousel-container"
-
-        >
-            {
-                bannerData.map((data, idx) =>(
-                  <Box key={idx}>
-                    <Image src={data.url} alt="banner image" />
-                    </Box>
-                ))
-            }
-        </Carousel>
-
-      </Box>
-        {/* banner ends here */}
 
 
 
